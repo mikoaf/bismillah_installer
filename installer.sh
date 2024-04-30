8f224c482c176191bca9bf6cc4ba14a5c0504675
mikoaf <mikoalfandi2801@gmail.com>
2024-04-30 09:13:29 +0700

    coba v1.2

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ then
     #Install venv & pip
     echo "Install venv & pip"
     sudo apt update -y
-    # sudo apt install python3-dev -y
+    sudo apt install python3-dev -y
     sudo apt install python3-venv -y
     sudo apt install python3-pip -y
     sleep 0.5

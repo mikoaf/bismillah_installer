52e3733e70de87e1b4cbda10f12a7bd8022429ea
mikoaf <mikoalfandi2801@gmail.com>
2024-05-06 11:35:11 +0700

    coba v1.40

--- sensor/dht.py
+++ sensor/dht.py
@@ -12,8 +12,7 @@ class Dht(DhtBase):
         try:
             suhu = self.dht11_sensor.temperature
             lemb = self.dht11_sensor.humidity
-            # return Response(status=StatusResponse.success, message="success"), DhtResult(humidity=lemb, temp=suhu)
-            return suhu, lemb
+            return Response(status=StatusResponse.success, message="success"), DhtResult(humidity=lemb, temp=suhu)
             # return suhu
         except RuntimeError as e:
             return Response(status=StatusResponse.error, message=str(e)), DhtResult(temp=0.0, humidity=0.0)
